647d9b2d7c522fb9ce6233e4bfe0c2f5552643a8
Elizabeth <elizabethcommon12@gmail.com>
2025-08-02 20:58:24 -0400

    dog

---
+++ index.js
@@ -0,0 +1,6 @@
+async function main() {
+    const dogs = await fetch("https://api.thedogapi.com/v1/images/search?has_breeds=true&limit=25&api_key=live_46jUtRQ0tvjuWjtXLgxENhXe42bjJ1StbjSuNNqHkeRnEN1zc2Fg9G6gklIyheDw");
+    console.log (dogs);
+}
+
+main();
