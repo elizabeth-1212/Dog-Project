6884ff078e7d9400f7cab1c40aa8e9947b3cd517
Elizabeth <elizabethcommon12@gmail.com>
2025-08-04 22:38:25 -0400

    dogs1

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 async function main() {
     const dogs = await fetch("https://api.thedogapi.com/v1/images/search?has_breeds=true&limit=25&api_key=live_46jUtRQ0tvjuWjtXLgxENhXe42bjJ1StbjSuNNqHkeRnEN1zc2Fg9G6gklIyheDw");
-    const dogsData = await dogs.json()
-    console.log (dogsData.breeds);
+    const dogsData = await dogs.json()   
+        dogsData.map( dog => `<div class="search__container">
+            <div class="breed-list">
+            <div class="breed-card">
+            <h1><b>Breed name</b></h1>
+            <h4><b>Bred for:</b></h4>
+            <h4><b>Life span:</b></h4>
+            <h4><b>Temperament:</b></h4>
+            <h4><b>Photo</b></h4>
+            </div>
+            </div>
+            </div>`
+        )
+        .join("")
+
 }
 
 main();
